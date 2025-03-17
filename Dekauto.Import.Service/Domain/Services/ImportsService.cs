da54a24344f9bbd9e20d31b9eb911dfe63da27eb
JSamuraiL <alex.ermiloff2017@yandex.ru>
2025-03-17 23:47:00 +0300

    Добавлены другие типы населенных пунктов

--- Dekauto.Import.Service/Domain/Services/ImportsService.cs
+++ Dekauto.Import.Service/Domain/Services/ImportsService.cs
@@ -59,7 +59,7 @@ namespace Dekauto.Import.Service.Domain.Services
                                         {
                                             student.EducationRelationDate = DateTime.Parse(cellValue.ToString());
                                             student.EducationStartYear = short.Parse(student.EducationRelationDate.Value.Year.ToString());
-                                            student.EducationFinishYear = (short)(student.EducationStartYear - student.EducationTime);
+                                            student.EducationFinishYear = (short)(student.EducationStartYear + student.EducationTime);
                                         }
                                         break;
                                     case "№ договора":
@@ -108,6 +108,7 @@ namespace Dekauto.Import.Service.Domain.Services
                         var student = new Student();
                         string courseOfTraining = string.Empty;
 
+                        // Фиксированные данные
                         student.Education = "высшее"; // других видов нет
                         student.EducationForm = "очная"; // других форм нет
                         student.Faculty = "Информационных технологий"; // Т.к. делаем прогу для нашего факультета, факультет будет такой. Пока взять альтернативные варианты неоткуда
@@ -115,6 +116,10 @@ namespace Dekauto.Import.Service.Domain.Services
                         student.EducationBase = "бюджетная (ФБ)";// других основ нет
                         student.EducationRelationForm = "договор об образовании на обучение";// платных услуг нет
                         student.EducationTime = 4; // Пока ставль 4, т.к. неоткуда брать данные
+                        student.LivingInDormitory = false; // Общежитие по умолчанию нет
+                        student.MilitaryService = false; // Служба в армии по умолчанию нет
+                        student.MaritalStatus = false; // Отношения по умолчанию отсутствуют
+
 
                         for (int col = 1; col <= columnCount; col++) 
                         {
@@ -123,11 +128,11 @@ namespace Dekauto.Import.Service.Domain.Services
 
                             string indexPattern = @"\b\d{6}\b";
                             string addressTypePattern = @"\b(?:\w+)\s+([г|с])\b(?:\,)";
-                            string cityPattern = @"\b(\w+)\s+(?:[г|с]\,)"; // Пока что тригер работает только на города и села, для увеличения вариантов нужно идти в деканат
+                            string cityPattern = @"\b(\w+)\s+(?:[г|с|х|д|п]\,)"; // Пока что тригер работает только на города и села, для увеличения вариантов нужно идти в деканат
                             string housePattern = @"\b(?:д\.)\s+(\w+)\b(?:,)?";
                             string streetPattern = @",\s*([^,]*?)\s*,\s+д\.";
-                            string housingTypePattern = @"\b([к|с])\.\w*?"; // Нужно уточнение, как обозначается строение
-                            string housingPattern = @"[к|с]\.\s+(\w+)";
+                            string housingTypePattern = @"\b([к|стр])\.\w*?"; // Нужно уточнение, как обозначается строение
+                            string housingPattern = @"[к|стр]\.\s+(\w+)";
                             string apartementPattern = @"кв\.\s+(\w+)";
                             string numConcursPattern = @"(\d{2}\.\d{2}\.\d{2})";
 
@@ -166,7 +171,7 @@ namespace Dekauto.Import.Service.Domain.Services
                                 case "овддокумента удостоверяющего личность":
                                     student.PassportIssuancePlace = cellValue.ToString();
                                     break;
-                                case "код подразделения": // Здесь нужны уточнения, как называется поле
+                                case "код подразделения документа удостоверяющего личность": // Здесь нужны уточнения, как называется поле
                                     student.PassportIssuanceCode = cellValue.ToString();
                                     break;
                                 case "дата выдачи паспорта": // Здесь нужны уточнения, как называется поле и существует ли вообще
@@ -193,10 +198,19 @@ namespace Dekauto.Import.Service.Domain.Services
                                     switch (Regex.Match(cellValue.ToString(), addressTypePattern).Groups[1].ToString())
                                     {
                                         case "г":
-                                            student.AddressRegistrationType = "город";
+                                            student.AddressRegistrationType = "город:";
                                             break;
                                         case "с":
-                                            student.AddressRegistrationType = "село";
+                                            student.AddressRegistrationType = "село:";
+                                            break;
+                                        case "х":
+                                            student.AddressRegistrationType = "хутор:";
+                                            break;
+                                        case "д":
+                                            student.AddressRegistrationType = "деревня:";
+                                            break;
+                                        case "п":
+                                            student.AddressRegistrationType = "посёлок:";
                                             break;
                                     }
                                     student.AddressRegistrationHouse = Regex.Match(cellValue.ToString(), housePattern).Groups[1].ToString();
@@ -204,7 +218,7 @@ namespace Dekauto.Import.Service.Domain.Services
                                     if (Regex.Match(cellValue.ToString(), housingTypePattern).Groups[1].ToString().Trim() == "к") 
                                         student.AddressRegistrationHousingType = "корпус";
                                     else
-                                        if (Regex.Match(cellValue.ToString(), housingTypePattern).Groups[1].ToString().Trim() == "с") 
+                                        if (Regex.Match(cellValue.ToString(), housingTypePattern).Groups[1].ToString().Trim() == "стр") 
                                             student.AddressRegistrationHousingType = "строение";
                                     student.AddressRegistrationHousing = Regex.Match(cellValue.ToString(), housingPattern).Groups[1].ToString().Trim();
                                     student.AddressRegistrationApartment = Regex.Match(cellValue.ToString(), apartementPattern).Groups[1].ToString().Trim();
@@ -216,10 +230,19 @@ namespace Dekauto.Import.Service.Domain.Services
                                     switch(Regex.Match(cellValue.ToString(), addressTypePattern).Groups[1].ToString()) 
                                     {
                                         case "г":
-                                            student.AddressResidentialType = "город";
+                                            student.AddressResidentialType = "город:";
                                             break;
                                         case "с":
-                                            student.AddressResidentialType = "село";
+                                            student.AddressResidentialType = "село:";
+                                            break;
+                                        case "х":
+                                            student.AddressResidentialType = "хутор:";
+                                            break;
+                                        case "д":
+                                            student.AddressResidentialType = "деревня:";
+                                            break;
+                                        case "п":
+                                            student.AddressResidentialType = "посёлок:";
                                             break;
                                     }
                                     student.AddressResidentialHouse = Regex.Match(cellValue.ToString(), housePattern).Groups[1].ToString();
@@ -227,7 +250,7 @@ namespace Dekauto.Import.Service.Domain.Services
                                     if (Regex.Match(cellValue.ToString(), housingTypePattern).Groups[1].ToString().Trim() == "к")
                                         student.AddressResidentialHousingType = "корпус";
                                     else
-                                        if (Regex.Match(cellValue.ToString(), housingTypePattern).Groups[1].ToString().Trim() == "с")
+                                        if (Regex.Match(cellValue.ToString(), housingTypePattern).Groups[1].ToString().Trim() == "стр")
                                         student.AddressResidentialHousingType = "строение";
                                     student.AddressResidentialHousing = Regex.Match(cellValue.ToString(), housingPattern).Groups[1].ToString().Trim();
                                     student.AddressResidentialApartment = Regex.Match(cellValue.ToString(), apartementPattern).Groups[1].ToString().Trim();
@@ -256,6 +279,9 @@ namespace Dekauto.Import.Service.Domain.Services
                                         student.EducationReceived = "среднее общее образование";
                                     else if (cellValue.ToString().ToLower() == "диплом")
                                         student.EducationReceived = "среднее профессиональное образование";
+                                    break;
+                                case "образовательное учреждение":
+                                    student.OOName = cellValue.ToString();
                                     break;
                             }
                         }
