6f8cc52b9a4c7032ea123fb903151356b047b1be
TOXYGENCY <delorean2026@ya.ru>
2025-05-27 10:46:33 +0300

    фикс program.cs

--- Dekauto.Import.Service/API/Program.cs
+++ Dekauto.Import.Service/API/Program.cs
@@ -56,7 +56,7 @@ try
         });
 
         c.AddSecurityRequirement(new OpenApiSecurityRequirement
-        {
+    {
         {
             new OpenApiSecurityScheme
             {
@@ -68,12 +68,12 @@ try
             },
             new string[] {}
         }
-        });
+    });
     });
     builder.Services.AddTransient<IImportService, ImportsService>();
     builder.Services.AddSingleton<IRequestMetricsService, RequestMetricsService>();
     builder.Services.AddScoped<Mutation>();
-    // Включаем межсервисную авторизацию по конфигу
+    // Âêëþ÷àåì ìåæñåðâèñíóþ àâòîðèçàöèþ ïî êîíôèãó
     if (Boolean.Parse(builder.Configuration["UseEndpointAuth"] ?? "true"))
     {
         builder.Services
@@ -82,7 +82,7 @@ try
             "Basic",
             options => { });
 
-        // Общая политика (можно использовать и для GraphQL и для обычных endpoints)
+        // Îáùàÿ ïîëèòèêà (ìîæíî èñïîëüçîâàòü è äëÿ GraphQL è äëÿ îáû÷íûõ endpoints)
         builder.Services.AddAuthorization(options =>
         {
             options.DefaultPolicy = new AuthorizationPolicyBuilder("Basic")
@@ -92,14 +92,14 @@ try
     }
     else
     {
-        // Заглушка политик доступа, если авторизация выключена
+        // Çàãëóøêà ïîëèòèê äîñòóïà, åñëè àâòîðèçàöèÿ âûêëþ÷åíà
         builder.Services.AddAuthorizationBuilder()
         .SetDefaultPolicy(new AuthorizationPolicyBuilder()
-        .RequireAssertion(_ => true) // Всегда разрешаем доступ
+        .RequireAssertion(_ => true) // Âñåãäà ðàçðåøàåì äîñòóï
         .Build());
     }
 
-    // Включаем GraphQL по конфигу
+    // Âêëþ÷àåì GraphQL ïî êîíôèãó
     if (Boolean.Parse(builder.Configuration["UseGraphQL"] ?? "true"))
     {
         builder.Services
@@ -126,60 +126,60 @@ try
 
     var app = builder.Build();
 
-app.UseCors("AllowAll");
-
-if (Boolean.Parse(builder.Configuration["UseGraphQL"] ?? "true"))
-{
-    app.MapGraphQL();
-    Log.Information("Enabled GraphQL.");
-}
-
-// Включаем межсервисную авторизацию (в том числе через [Authorize])
-if (Boolean.Parse(app.Configuration["UseEndpointAuth"] ?? "true"))
-{
-    app.UseAuthentication();
-    app.UseAuthorization();
-    Log.Information("Enabled basic authorization.");
+    app.UseCors("AllowAll");
 
     if (Boolean.Parse(builder.Configuration["UseGraphQL"] ?? "true"))
     {
-        app.MapGraphQL().RequireAuthorization();
-        Log.Information("Enabled GraphQL with authorization.");
+        app.MapGraphQL();
+        Log.Information("Enabled GraphQL.");
     }
-}
-else
-{
-    Log.Warning("Disabled authorization.");
-}
+
+    // Âêëþ÷àåì ìåæñåðâèñíóþ àâòîðèçàöèþ (â òîì ÷èñëå ÷åðåç [Authorize])
+    if (Boolean.Parse(app.Configuration["UseEndpointAuth"] ?? "true"))
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+        Log.Information("Enabled basic authorization.");
+
+        if (Boolean.Parse(builder.Configuration["UseGraphQL"] ?? "true"))
+        {
+            app.MapGraphQL().RequireAuthorization();
+            Log.Information("Enabled GraphQL with authorization.");
+        }
+    }
+    else
+    {
+        Log.Warning("Disabled authorization.");
+    }
 
 
     // Configure the HTTP request pipeline.
 
-    // Явно указываем порты (для Docker)
+    // ßâíî óêàçûâàåì ïîðòû (äëÿ Docker)
     app.Urls.Add("http://*:5503");
 
-if (app.Environment.IsDevelopment())
-{
-    Log.Warning("Development version of the application is started. Swagger activation...");
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (app.Environment.IsDevelopment())
+    {
+        Log.Warning("Development version of the application is started. Swagger activation...");
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-// Включаем https, если указано в конфиге
-if (Boolean.Parse(app.Configuration["UseHttps"] ?? "false"))
-{
-    app.Urls.Add("https://*:5504");
-    app.UseHttpsRedirection();
-    Log.Information("Enabled HTTPS.");
-}
-else
-{
-    Log.Warning("Disabled HTTPS.");
-}
+    // Âêëþ÷àåì https, åñëè óêàçàíî â êîíôèãå
+    if (Boolean.Parse(app.Configuration["UseHttps"] ?? "false"))
+    {
+        app.Urls.Add("https://*:5504");
+        app.UseHttpsRedirection();
+        Log.Information("Enabled HTTPS.");
+    }
+    else
+    {
+        Log.Warning("Disabled HTTPS.");
+    }
 
     app.MapControllers();
 
-    app.UseMetricsMiddleware(); // Метрики
+    app.UseMetricsMiddleware(); // Ìåòðèêè
 
     app.Run();
 }
